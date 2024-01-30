eb9cddc42a57c66b0e672a57920317dd5b5fb71e
Tkachev Maksim <maks93makula@gmail.com>
2024-01-30 18:57:20 +0600

    основной компонент приложения. получает сообщение с сервера и отправляет новые

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import Chat from './components/Chat/Chat';
+import {Messages} from "./type";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+    const [messages, setMessages] = useState<Messages[]>([]);
+    const [dateTime, setDateTime] = useState<string>('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    useEffect(() => {
+        const fetchMessages = async () => {
+            try {
+                const url = `http://146.185.154.90:8000/messages${dateTime && `?datetime=${dateTime}`}`;
 
-export default App
+
+                const response = await fetch(url);
+                const data = await response.json();
+
+                if (data.length > 0) {
+                    setMessages(prevMessages => [
+                        ...prevMessages,
+                        ...data
+                    ]);
+
+                    setDateTime(data[data.length - 1].datetime);
+                }
+            } catch (error) {
+                console.error('Ошибка:', error);
+            }
+        };
+
+
+        const interval = setInterval(fetchMessages, 3000);
+
+        return () => clearInterval(interval);
+    },[dateTime]);
+
+    const submit = async (author: string, message: string) => {
+        try {
+            const messageText = new URLSearchParams();
+            messageText.append('author', author);
+            messageText.append('message', message);
+
+            await fetch('http://146.185.154.90:8000/messages', {
+                method: 'POST',
+                body: messageText,
+            });
+        } catch (error) {
+            console.error('Ошибка с сообщением:', error);
+        }
+    };
+
+    return (
+        <div>
+            <Chat messages={messages} onSubmit={submit} />
+        </div>
+    );
+};
+
+export default App;
